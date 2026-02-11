7dc0723eeeaa47907aabeb47ef05fb3f88101967
ᖫ✧ⓄⒼ ⓏⒺⓃ⊙.☉↤▀ <mandilaelvis9@gmail.com>
2026-02-11 04:01:38 +0300

    Update credentials with live keys

--- credentials.php
+++ credentials.php
@@ -2,6 +2,6 @@
 // credentials.php
 
 $credentials = [
-    'token'=>'',
-    'publishable_key'=>''
+    'token'=>'ISSecretKey_live_40891829-d33f-4b3d-bc5b-9101c3cbb2b8',
+    'publishable_key'=>'ISPubKey_live_1031eac9-008d-46a3-ac6b-44de2db6bc97'
 ];
